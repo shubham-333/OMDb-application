246c2c447221fd7cf4965f176f1b7843c22bc21c
Shubham Singla <shubhamsingla369@gmail.com>
2022-07-18 15:09:39 +0530

    backend completed

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-```npx express-generator --no-view omdb-backend```
-```cd omdb-backend```
-```npm install```
-```npm install -g nodemon```
-```npm run debug```
+```
+npx express-generator --no-view omdb-backend
+cd omdb-backend
+npm install
+npm install -g nodemon```
+npm run debug
+```
+
+```
+npx create-react-app my-app
+cd my-app
+npm start
+```
+
+localhost:3000/omdb/search/?title=[title]/
--- app-server/routes/omdb.js
+++ app-server/routes/omdb.js
@@ -5,21 +5,9 @@ var router = express.Router(); // Create a new router instance
 const OMDB_API_KEY = process.env.OMDB_API_KEY;
 const OMDB_URL = process.env.OMDB_URL;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  // Option 1
-  next() //function which will pass on to the next middleware
-  // Option 2
-  // res.send('Hello World'); 
-});
-
-router.get('/', function(req, res, next) {
-  res.send('I am next'); 
-});
-
 
 router.get('/search', (req, res, next) => {
-  const title = req.query.title;
+  const title = req.query.title; // get the title from the query string in the URL, Specific to Express
   const url = `${OMDB_URL}?apikey=${OMDB_API_KEY}&s=${title}`;
   // axios.get -> returns a promise.
   // On a promis -> you have to do .then(when data is available)
